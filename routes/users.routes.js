3801a6c433b330a181e502c0968fd5a03338397a
MarceloSias05 <marcelosias2005@gmail.com>
2025-04-28 23:22:40 -0600

    build: se realizo la creación de los metodos para hacer login, eliminar un usuario y modificar un usuario. Con metodos de HTTP, los cuales fueron, post, delete y put

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { sal, ping } from "../controllers/index.controllers.js";
-import { getUsers, postUsers} from "../controllers/user.controllers.js";
+import { getUsers, postUsers, login, deleteUser, putUser } from "../controllers/user.controllers.js";
+
 const router = Router();
 
 //Get methods
@@ -10,6 +11,17 @@ router.get("/get-users", getUsers); // Obtener todos los usuarios
 
 //post methods
 router.post("/post-users", postUsers);
+
+// Ruta para login
+router.post("/login", login);
+
+// Ruta para eliminar un usuario -> DELETE
+router.delete("/delete-users/:id", deleteUser);
+
+// Ruta para modificar un usuario  // PUT
+router.put("/update-user/:id", putUser);
+
 // Crear un nuevo usuario
 //router.post("/users", postUsers); // Crear un nuevo usuario (descomentado si se necesita)
 export default router;
+
